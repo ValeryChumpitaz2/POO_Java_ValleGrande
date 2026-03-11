1b95306d53f27abbaf0783fda9ca646a1aa5dd52
Valery Chumpitaz <valery,chumpitaz@vallegrande.edu.pe>
2026-03-11 08:49:35 -0500

    model

--- estructuraMVC/src/main/java/vallegrande/edu/pe/model/Contact.java
+++ estructuraMVC/src/main/java/vallegrande/edu/pe/model/Contact.java
@@ -6,7 +6,45 @@ import java.util.Objects;
  * Modelo que representa un contacto en la agenda.
  */
 public class Contact {
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
 
+    public Contact(String id, String name, String email, String phone  ) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+    //Getters
+    public String id(){
+        return id;
+    }
+    public String name(){
+        return name;
+    }
+    public String email(){
+        return email;
+    }
+
+    public String phone(){
+        return phone;
+    }
+
+    //Setters
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
 
 
     @Override
