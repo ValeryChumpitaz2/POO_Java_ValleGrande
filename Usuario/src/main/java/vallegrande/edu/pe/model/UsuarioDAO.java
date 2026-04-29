91c09cb8384aa7196ac596f9c4dbb66428540d0d
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-04-29 08:03:09 -0500

    delete

--- Usuario/src/main/java/vallegrande/edu/pe/model/UsuarioDAO.java
+++ Usuario/src/main/java/vallegrande/edu/pe/model/UsuarioDAO.java
@@ -7,4 +7,32 @@ import java.util.List;
 public class UsuarioDAO {
 
 
+    public List<Usuario> listar() {
+        List<Usuario> lista = new ArrayList<>();
+
+
+        try {
+            Connection con = ConexionBD.getConexion();
+            String sql = "SELECT * FROM usuario";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+
+            while (rs.next()) {
+                Usuario u = new Usuario();
+                u.setId(rs.getInt("id"));
+                u.setNombre(rs.getString("nombre"));
+                u.setCorreo(rs.getString("correo"));
+                lista.add(u);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return lista;
+    }
 }
+
